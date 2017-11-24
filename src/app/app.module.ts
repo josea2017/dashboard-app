804ac8f0116aa9f4b60ea6fe57fcf7fd2fec2fe2
jose <josea3712@gmail.com>
2017-11-23 19:35:50 -0600

    Agregando routing en el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+
 
 import { AppComponent } from './app.component';
 import { PersonasCrudComponent } from './personas-crud/personas-crud.component';
@@ -16,7 +18,8 @@ import { ProyectosCrudComponent } from './proyectos-crud/proyectos-crud.componen
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
